6b58d0d56b30604dd5591a10028e08e581300e89
Abdul-Samad Olagunju <abdulolagunju19@gmail.com>
2022-08-01 12:45:05 -0600

    make it better again

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import time
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'roon74'
 app.config['MYSQL_DB'] = 'crypto'
