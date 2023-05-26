5ae40f7cccca85f226244b86109bc8067c32df87
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2023-05-25 21:59:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
 app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
 app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
 app.config['MYSQL_CURSORCLASS'] = os.environ['MYSQL_CURSORCLASS']
-app.config['MYSQL_ATTR_SSL_CA'] = os.environ['MYSQL_ATTR_SSL_CA']
-app.config['MYSQL_DATABASE_URI'] = os.environ['MYSQL_DATABASE_URI']
+
+
 
 mysql = MySQL(app)
 
